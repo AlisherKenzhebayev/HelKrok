11588be257ce3a8d915df4966e69916b7314ce03
Martin Hromadka <martinhromadka7@gmail.com>
2022-06-16 20:40:42 +0200

    transition on death

--- Assets/Scripts/Classes/Subclasses/PlayerDamageTaker.cs
+++ Assets/Scripts/Classes/Subclasses/PlayerDamageTaker.cs
@@ -1,7 +1,19 @@
 using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using Cinemachine;
 
 public class PlayerDamageTaker : DamageTaker
 {
+    CinemachineVirtualCamera vcam;
+    float fovMultiplier = 100f;
+
+
+    private void Awake()
+    {
+        vcam = GameObject.FindGameObjectWithTag("Player").transform.parent.GetComponentInChildren<CinemachineVirtualCamera>();
+        print(vcam);
+    }
     private void Update()
     {
         EventManager.TriggerEvent("currentHealthPlayer", new Dictionary<string, object> { { "amount", this.FracHealth } });
@@ -10,8 +22,30 @@ public class PlayerDamageTaker : DamageTaker
     internal override void DoDeath()
     {
         //GameplayManager.ShowUiScreen();
-        GameplayManager.ResetCheckpoint();
+        StartCoroutine(DeathAfterTime());
         //GameplayManager.SaveGame();
         //SceneLoaderManager.LoadEnum(SceneLoaderManager.ScenesEnum.DeathScene);
     }
+
+    IEnumerator DeathAfterTime(float time = 1f)
+    {
+        float startFOV = vcam.m_Lens.FieldOfView;
+        for (float timer = 0; timer < time; timer += Time.deltaTime)
+        {
+            yield return null;
+            vcam.m_Lens.FieldOfView -= Time.deltaTime * fovMultiplier;
+        }
+
+
+        GameplayManager.ResetCheckpoint();
+        for (float timer = 0; timer < time; timer += Time.deltaTime)
+        {
+            yield return null;
+            vcam.m_Lens.FieldOfView += Time.deltaTime * fovMultiplier;
+        }
+
+        vcam.m_Lens.FieldOfView = startFOV;
+    }
+
+
 }
